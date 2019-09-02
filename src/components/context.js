6b87af0f628c64e70682d677e70eaef327a6c375
seth@mondido <sethnejame>
2019-09-02 20:28:17 +0200

    Adding context.js for Context API

---
+++ src/components/context.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const Context = React.createContext();
+
+export class Provider extends Component {
+    render() {
+        return (
+            <Context.Provider>
+
+
+            </Context.Provider>
+        )
+    }
+}
