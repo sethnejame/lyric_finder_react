836384eb8bfcdd66e3ae2084bedc7c707d3f3f42
seth@mondido <sethnejame>
2019-09-02 20:35:06 +0200

    Adding dummy data to context file

--- src/components/context.js
+++ src/components/context.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const Context = React.createContext();
 
 export class Provider extends Component {
-    render() {
-        return (
-            <Context.Provider>
+  state = {
+    track_list: [
+      { track: { track_name: "Waterfalls" } },
+      { track: { track_name: "Toms Diner" } }
+    ],
+    heading: "Top 10 Tracks"
+  };
 
-
-            </Context.Provider>
-        )
-    }
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {/* Whatever state we put in this provider
+        will be accessible by all other components 
+        as long as we use what's called the 'consumer' */}
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
+
+export const Consumer = Context.Consumer;
+// this is what we import to our component in order
+// to access state from the context, like { connect }
