8a6c80e7d7532a6dcd6de41493d37dafee4a18ce
seth@mondido <sethnejame>
2019-09-02 21:30:13 +0200

    Adding conditional @ Tracks component; if tracks have loaded, print conf, else show spinner comp

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { Consumer } from "./context";
+import Spinner from "./Spinner";
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>{value}</h1>;
+          const { track_list } = value;
+          // the above destructures the value object and
+          // 'pulls out' the track_list property
+          if (track_list === undefined || track_list.length === 0) {
+            return <Spinner />;
+          } else {
+            return <h1>Tracks loaded</h1>;
+          }
         }}
       </Consumer>
     );
