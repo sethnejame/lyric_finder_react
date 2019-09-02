4b9716ad4e26364a27d7fbfeef0ad1185deb0274
seth@mondido <sethnejame>
2019-09-02 20:42:33 +0200

    Wrapping App component in Provider, logging state from context.js inside Tracks.js (this is so much easier than Redux)

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Index from "./components/Index";
+import { Provider } from "./components/context";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Index} />
-          </Switch>
-        </div>
-      </>
-    </Router>
+    <Provider>
+      <Router>
+        <>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </>
+      </Router>
+    </Provider>
   );
 }
 
