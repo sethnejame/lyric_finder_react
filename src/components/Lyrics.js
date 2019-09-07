10da96aa37802210621dc3fc47fbb1592d79dac6
seth@mondido <sethnejame>
2019-09-07 08:11:02 +0200

    Lyrics now populate in Lyrics component, pulled in from musixmatch API

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Spinner from "./Spinner";
+import { Link } from "react-router-dom";
 
 class Lyrics extends Component {
   state = {
@@ -15,7 +17,6 @@ class Lyrics extends Component {
       )
       .then(res => {
         this.setState({ lyrics: res.data.message.body.lyrics });
-
         return axios.get(
           `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id})&apikey=${process.env.REACT_APP_MM_KEY}`
         );
@@ -27,11 +28,32 @@ class Lyrics extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+    const { track, lyrics } = this.state;
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+        </>
+      );
+    }
   }
 }
 
